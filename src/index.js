0dc9e37ea0c31e3211bc0692f8de417d49bd9772
ththlisa <ththlisa@gmail.com>
2018-09-03 20:44:45 -0700

    gist

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeNumber from './components/primeNumber';
+import RenderGist from 'react-render-gist';
 
 
 
@@ -14,12 +15,20 @@ class MyForm extends React.Component {
     return (
       <div className="container h-100" >
         <div className="row h-100 justify-content-center align-items-center">
-          <div className="card col-lg-6 text-center">
+          <div className="card col-lg-7 text-center mt-5">
             <div className="card-body">
               <h5 className="card-title">Find the Prime Factors</h5>
               <div className="form-group">
                 <PrimeNumber />
               </div>
+            </div>
+
+          </div>
+          <div className="card col-lg-7 text-center m-4">
+            <div className="card-body">
+              <h5 className="card-text">The algorithm of finding prime factors in reactjs</h5>
+              <div className="form-group">
+                <RenderGist gist="24114643bb61eb07eddaadc0bd453fb4" /></div>
             </div>
           </div>
         </div>
